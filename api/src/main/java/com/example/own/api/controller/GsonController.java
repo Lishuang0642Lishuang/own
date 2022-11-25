2acca3af9e27ccc9c68c55db61b38ba3fc7dfa15
yingbu <yingbu.li@tuya.com>
2022-11-26 01:39:00 +0800

    ADD:新增redis

---
+++ api/src/main/java/com/example/own/api/controller/GsonController.java
@@ -0,0 +1,27 @@
+package com.example.own.api.controller;
+
+import com.example.own.core.mongo.entity.ChangeHistoryDO;
+import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+public class GsonController {
+
+
+
+    @ResponseBody
+    @RequestMapping("/gson")
+    public List<ChangeHistoryDO> getChangeHistoryDOList() {
+
+        ChangeHistoryDO changeHistoryDO = new ChangeHistoryDO();
+        changeHistoryDO.setCode("sdf");
+
+        String gsonString = new Gson().toJson(changeHistoryDO);
+
+        new Gson().fromJson(gsonString, ChangeHistoryDO.class);
+
+        return null;
+    }
+}
