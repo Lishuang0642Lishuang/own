e89048c14fd4b2b018a027b95db10017707bedb9
lishuang <link.li@ecoflow.com>
2024-01-12 14:26:31 +0800

    应用的枚举

--- service/src/main/java/com/example/own/service/calculateCase/ApplicationEnum.java
+++ service/src/main/java/com/example/own/service/calculateCase/ApplicationEnum.java
@@ -11,7 +11,20 @@ public enum ApplicationEnum {
 
 
 
-//    IOT_SERVICE("iot", "iot-service"),
+    USER("user", "ecoflow-user\\ecoflow-service-user\\target\\classes\\", "ecoflow-user\\ecoflow-service-user\\target\\classes\\com\\ecoflow\\service\\user\\controller"),
+    IOT_SMART_VOICE("iot-smart-voice", "ecoflow-iot-smart-voice\\smart-voice-service\\target\\classes\\","ecoflow-iot-smart-voice\\smart-voice-service\\target\\classes\\com\\ecoflow\\iot\\smartvoice\\controller"),
+    IOT_SCENE("iot-scene", "ecoflow-iot-scene\\ecoflow-iot-scene-service\\target\\classes\\", "ecoflow-iot-scene\\ecoflow-iot-scene-service\\target\\classes\\com\\ecoflow\\iot\\scene\\controller"),
+    IOT_OTA("iot-ota", "ecoflow-iot-ota\\ota-service\\target\\classes\\", "ecoflow-iot-ota\\ota-service\\target\\classes\\com\\ecoflow\\iot\\ota\\controller"),
+    IOT_OPEN("iot-open", "ecoflow-iot-open\\open-service\\target\\classes\\", "ecoflow-iot-open\\open-service\\target\\classes\\com\\ecoflow\\iot\\open\\controller"),
+    IOT_DEVICE("iot-devices", "ecoflow-iot-devices\\devices-service\\target\\classes\\","ecoflow-iot-devices\\devices-service\\target\\classes\\com\\ecoflow\\iot\\devices\\controller"),
+    GENERAL("general", "ecoflow-general\\general-service\\target\\classes\\","ecoflow-general\\general-service\\target\\classes\\com\\ecoflow\\general\\controller"),
+    EXPORT("export","ecoflow-export\\export-service\\target\\classes\\","ecoflow-export\\export-service\\target\\classes\\com\\ecoflow\\export\\controller"),
+    DATA("data-service", "ecoflow-data\\data-service\\target\\classes\\","ecoflow-data\\data-service\\target\\classes\\com\\ecoflow\\data\\controller"),
+    CMS("cms", "ecoflow-cms\\cms-service\\target\\classes\\","ecoflow-cms\\cms-service\\target\\classes\\com\\ecoflow\\cms\\controller"),
+    BATTERY("battery-service", "ecoflow-battery\\battery-service\\target\\classes\\","ecoflow-battery\\battery-service\\target\\classes\\com\\ecoflow\\battery\\controller"),
+    AUTH("auth","ecoflow-auth\\target\\classes\\","ecoflow-auth\\target\\classes\\com\\ecoflow\\auth\\controller"),
+    IOT_AUTH("iot-auth", "ecoflow-iot-auth\\auth-service\\target\\classes\\","ecoflow-iot-auth\\auth-service\\target\\classes\\com\\ecoflow\\iot\\auth\\controller"),
+    IOT_SERVICE("iot-service","ecoflow-iot\\iot-service\\iot-service\\target\\classes\\", "ecoflow-iot\\iot-service\\iot-service\\target\\classes\\com\\ecoflow\\iot\\controller"),
     APP("app", "ecoflow-app\\app-service\\target\\classes\\","ecoflow-app\\app-service\\target\\classes\\com\\ecoflow\\app\\controller"),
     ;
 
